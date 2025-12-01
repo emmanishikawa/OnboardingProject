6c3fcf504fc463e9a8ac5dba4c9deddf68e07a36
emmanishikawa <emmanishikawa@gmail.com>
2025-11-30 23:31:08 -0800

    added text and placeholder image

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,23 @@ export default function Home() {
     return (
         <>
         {/**name brief intro interests, pic */}
-        
-        <div className="flex h-screen w-screen justify-center items-center bg-[#d8e4eb]">
-                <div className="flex h-[600px] w-[350px] 
-                    md:h-[350px] md:w-[600px] 
-                        bg-[#f7fbfc]">
+        <div className="flex h-screen w-screen justify-center items-center 
+            bg-[#d8e4eb]">
+            <div className="flex flex-row h-[600px] w-[350px] 
+                        md:h-[350px] md:w-[600px] 
+                            p-5
+                            bg-[#f7fbfc] rounded-md drop-shadow-sm">
+                <div className="flex flex-col">
+                    <h1 className="text-[22px]">emma nishikawa</h1>
+                    <p className="">second year / sixth</p>
+                    <p className="">major: cognitive science - ml & nc</p>
+                    <p className="">minor: computer science</p>
+                </div>
+                <div>
+                    <img src="" alt="profile photo"/>pic
                 </div>
             </div>
+        </div>
         </>
     );
 }
