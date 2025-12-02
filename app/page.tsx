15aada498eaee6f1d4ee306501a0c965a8bd6af3
emmanishikawa <emmanishikawa@gmail.com>
2025-12-01 21:36:56 -0800

    added comments

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import profileImage from "./assets/pfp.jpeg";
 export default function Home(){
     return (
         <>
-        {/**name brief intro interests, pic */}
+        {/* background and content container */}
         <div className='flex h-screen w-screen justify-center items-center 
             bg-[#d8e4eb]'>
             <div className='flex flex-col md:flex-row 
                             h-[550px] w-[350px] md:h-[350px] md:w-[600px] 
                             p-5
                             bg-[#f7fbfc] rounded-md drop-shadow-sm'>
+                {/* inner container and texts*/}
                 <div className='flex flex-col h-1/2 md:h-full w-full
                                 text-[20px] text-blue-950'>
                     <h1 className='text-[24px]'>emma nishikawa</h1>
@@ -19,6 +20,7 @@ export default function Home(){
                     <p className=''>minor: computer science</p>
                     <p className=''>interests: listening to music, travelling, matcha 🍵</p>
                 </div>
+                {/* profile image */}
                 <div className='flex h-full w-full justify-center items-center'>
                     <img src='/pfp.jpeg' className='w-[280px] md:w-96'/>
                 </div>
