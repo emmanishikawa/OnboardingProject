66f23de79af57594653984cb94d53a264594f1e3
emmanishikawa <emmanishikawa@gmail.com>
2025-12-28 09:58:55 -0800

    move nav components to own folder

--- components/Navigation/navbar.tsx
+++ components/Navigation/navbar.tsx
@@ -1,7 +1,4 @@
-import Home from "@/app/page";
 import NavButton from "./navbutton";
-import Experience from "@/app/experience/page";
-import Projects from "@/app/project/page";
 
 export default function NavBar() {
     return (
@@ -11,7 +8,7 @@ export default function NavBar() {
                         fixed z-10 
                         bg-white/50 backdrop-blur-sm border-[1px] border-white'>
                 <div className='flex flex-row gap-5
-                                md:w-[800px] p-5
+                                w-full md:w-[800px] p-5
                                 text-[18px] decoration-wavy'>
                     <NavButton destinaton=" " children="Home" />
                     <NavButton destinaton="experience" children="Experience" />
