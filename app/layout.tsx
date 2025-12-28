4b00e81b5ff9f0526102467df353d5b0426fdd94
emmanishikawa <emmanishikawa@gmail.com>
2025-12-28 09:57:37 -0800

    edit styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "./globals.css";
-import NavBar from "@/components/navbar";
+import NavBar from "@/components/Navigation/navbar";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import NavBar from '@/components/navbar';
 
 export default function Home(){
     return (
         <>
-        <NavBar />
-        {/* background and content container */}
-        <div className='flex flex-col h-screen w-screen justify-center items-center mt-32'>
-            <div className='flex flex-col md:flex-row 
-                            h-full w-full md:w-[800px] 
-                            p-5 md: md:rounded-md
-                            drop-shadow-md'>
-                {/* inner container and texts*/}
-                <div className='flex flex-col h-1/2 md:h-full w-full
-                                text-[20px] text-blue-950'>
-                    <h1 className='text-[24px]'>Emma Nishikawa</h1>
-                    <p>Second year / Sixth</p>
-                    <p>Major: Cognitive Science - ML & NC</p>
-                    <p>Minor: Computer Science</p>
-                    <p>Interests: Listening to music, travelling, matcha 🍵</p>
+        <div className='flex flex-col h-full w-screen justify-center items-center mt-40'>
+            <div className='flex flex-col md:flex-row gap-8 h-full w-full md:w-[800px] p-5 md: md:rounded-md'>
+                <div className='flex items-start'>
+                    <img src='/pfp.jpeg' 
+                         className='w-52 aspect-square object-cover rounded-md'/>
                 </div>
-                {/* profile image */}
-                <div className='flex h-full w-full items-start'>
-                    <img src='/pfp.jpeg' className='object-contain'/>
+                <div className='flex flex-col h-full grow text-[16px]'>
+                    <h1 className='mb-3 text-[20px]'>emma nishikawa</h1>
+                    <p>i am an undergraduate at ucsd majoring in cognitive science: machine learning
+                         with a minor in computer science.</p>
+                    <p>my interests include listening to music, travelling, and matcha : &#41;</p>
                 </div>
             </div>
         </div>
