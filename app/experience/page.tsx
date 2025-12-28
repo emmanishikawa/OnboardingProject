44c6dcba1e94bd15cd04487df0162beed44509cb
emmanishikawa <emmanishikawa@gmail.com>
2025-12-28 10:53:23 -0800

    experience page formatting

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -5,13 +5,13 @@ import ExperienceComponent from "@/components/Experience/experiences_component"
 export default function Experiences() {
     return (
         <>
-        <div className='flex flex-col h-screen w-screen justify-center items-center mt-32'>
-            <div className='flex flex-col
-                            h-full w-full md:w-[800px] 
-                            p-5 md: md:rounded-md'>
-              <ExperienceComponent />  
+            <div className='flex flex-col h-screen w-screen justify-center items-center mt-32'>
+                <div className='flex flex-col
+                                h-full w-full md:w-[800px] 
+                                p-5 md: md:rounded-md'>
+                <ExperienceComponent />  
+                </div>
             </div>
-        </div>
         </>
     )
 }
